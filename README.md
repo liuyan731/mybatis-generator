4a86f6b8ac26690185ba5360a2ea6e07e0aa775c
liuyan <liuyan990731@gmail.com>
2016-06-16 17:36:46 +0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# mybatis-generator
+mybatis generator
+
+使用方法：java -jar mybatis-generator-core-1.3.2.jar -configfile generatorConfig.xml -overwrite
---
+++ src/test/model/Product.java
@@ -0,0 +1,33 @@
+package test.model;
+
+public class Product {
+    private Integer id;
+
+    private String name;
+
+    private String price;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price == null ? null : price.trim();
+    }
+}
